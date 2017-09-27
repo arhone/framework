09c359e7d5f745ca0ce862f5e90fe5eb7c23a13e
AlekseyArh <a@arh.one>
2017-09-27 15:55:40 +0300

    Framework

--- config/builder/instruction.php
+++ config/builder/instruction.php
@@ -4,5 +4,8 @@ return [
     'Controller' => [
         'require' => __DIR__ . '/../../vendor/arhone/framework/Controller.php',
         'class' => 'arhone\framework\Controller'
+    ],
+    'Tpl' => [
+        'class' => 'arhone\tpl\Tpl'
     ]
 ];
--- web/index.php
+++ web/index.php
@@ -4,4 +4,5 @@ include __DIR__ . '/../vendor/autoload.php';
 $Builder = new arhone\builder\Builder(include __DIR__ . '/../config/builder/config.php');
 $Builder->instruction(include __DIR__ . '/../config/builder/instruction.php');
 
-echo $Builder->make('Controller')->run($_REQUEST['uri'] ?? '/');
+$Tpl = $Builder->make('Tpl');
+echo $Tpl->get('index.tpl');
