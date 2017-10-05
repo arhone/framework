6fdd578c8f0f857411b340d97b1a441c5e19778b
Алексей Арх <info@arh.one>
2017-10-05 17:45:14 +0300

    framework

--- web/dev.php
+++ web/dev.php
@@ -2,4 +2,16 @@
 include __DIR__ . '/../vendor/autoload.php';
 
 $Builder = new arhone\builder\Builder();
+$Builder->instruction(include __DIR__ . '/../config/builder.php');
+
 $Trigger = $Builder->make('Trigger');
+
+$Trigger->handler('cache:(key)', function ($match, $data) {
+    return $match[1];
+});
+
+$Trigger->handler('cache:(key)', function ($match, $data) {
+    return $data . ' Юзер';
+});
+
+echo $Trigger->event('cache:key', 'данные');
--- web/index.php
+++ web/index.php
@@ -2,6 +2,4 @@
 include __DIR__ . '/../vendor/autoload.php';
 
 $Builder = new arhone\builder\Builder();
-//$Builder->instruction(include __DIR__ . '/../config/builder.php');
-
-$Trigger = $Builder->make('Trigger');
+$Builder->instruction(include __DIR__ . '/../config/builder.php');
