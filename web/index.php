acbf2f82d8ed0d29563b6e6e4ab802e7f4476faa
Алексей Арх <info@arh.one>
2017-10-05 17:19:55 +0300

    dev

--- web/index.php
+++ web/index.php
@@ -5,3 +5,23 @@ $Builder = new arhone\builder\Builder(include __DIR__ . '/../config/builder/conf
 $Builder->instruction(include __DIR__ . '/../config/builder/instruction.php');
 
 echo $Builder->make('Controller')->run($_REQUEST['uri'] ?? '/');
+
+class Test {
+
+    public function get () {
+        return 'Тест';
+    }
+
+}
+
+class Alias {
+    protected $Test;
+    public function __construct(Test $Test) {
+        $this->Test = $Test;
+    }
+    public function get () {
+        return $this->Test->get();
+    }
+}
+
+echo $Builder->make('Alias')->get();
