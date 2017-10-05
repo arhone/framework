96814c1455bc014f3f4c97afc4b3860290534c99
Алексей Арх <info@arh.one>
2017-10-05 17:54:01 +0300

    framework

--- web/dev.php
+++ web/dev.php
@@ -6,12 +6,12 @@ $Builder->instruction(include __DIR__ . '/../config/builder.php');
 
 $Trigger = $Builder->make('Trigger');
 
-$Trigger->handler('cache:(key)', function ($match, $data) {
+$Trigger->handler('cache:([а-я]+)', function ($match, $data) {
     return $match[1];
 });
 
-$Trigger->handler('cache:(key)', function ($match, $data) {
+$Trigger->handler('cache:([а-я]+)', function ($match, $data) {
     return $data . ' Юзер';
 });
 
-echo $Trigger->event('cache:key', 'данные');
+echo $Trigger->event('cache:Привет', 'данные');
