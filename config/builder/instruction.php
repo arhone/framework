5e7fd3c3310d743db70d68ed4ceb4f4bea108eb6
Алексей Арх <info@arh.one>
2017-10-05 17:21:55 +0300

    Merge branch 'master' into dev
    # Conflicts:
    #	web/index.php

--- config/builder/instruction.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'Controller' => [
-        'require' => __DIR__ . '/../../vendor/arhone/framework/Controller.php',
-        'class' => 'arhone\framework\Controller'
-    ]
-];
--- web/index.php
+++ web/index.php
@@ -1,27 +1,6 @@
 <?php
 include __DIR__ . '/../vendor/autoload.php';
 
-$Builder = new arhone\builder\Builder(include __DIR__ . '/../config/builder/config.php');
-$Builder->instruction(include __DIR__ . '/../config/builder/instruction.php');
-
-echo $Builder->make('Controller')->run($_REQUEST['uri'] ?? '/');
-
-class Test {
-
-    public function get () {
-        return 'Тест';
-    }
-
-}
-
-class Alias {
-    protected $Test;
-    public function __construct(Test $Test) {
-        $this->Test = $Test;
-    }
-    public function get () {
-        return $this->Test->get();
-    }
-}
-
-echo $Builder->make('Alias')->get();
+$Builder = new arhone\builder\Builder();
+$Builder->instruction(include __DIR__ . '/../config/builder.php');
+$Builder->make('Controller')->run('HTTP:GET:/');
