4d0a95636c8564680d33801e03bad45d041e02df
Алексей Арх <info@arh.one>
2017-11-18 13:27:09 +0300

    Framework

--- core/start.php
+++ core/start.php
@@ -1,5 +1,4 @@
 <?php
-mb_internal_encoding('utf-8'); // Кодировка
 ini_set('error_reporting', E_ALL | E_STRICT); // Типы ошибок, на которые надо ругаться
 ini_set('display_errors', false); // Вывод ошибок
 ini_set('error_log', __DIR__ . '/../log/error.log'); // Файл для сохранения ошибок
