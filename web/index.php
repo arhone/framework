cfc7fb3387e7af4875eff9bd24cf34061ed019e0
AlekseyArh <a@arh.one>
2017-11-20 13:16:27 +0300

    Framework

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 include __DIR__ . '/../core/start.php';
 
-$Builder = new arhone\builder\Builder();
+$Builder = new \arhone\builder\Builder();
 $Builder->instruction(include __DIR__ . '/../config/builder.php');
 
 echo $Builder->make('Controller')->run('HTTP:' . $_SERVER['REQUEST_METHOD'] . ':' . $_SERVER['REQUEST_URI'] ?? '/');
