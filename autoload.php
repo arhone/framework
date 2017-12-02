37b731fcd1922982ced20ce77a1c9a35664e3c52
Алексей Арх <info@arh.one>
2017-12-02 16:18:58 +0300

    Framework

--- autoload.php
+++ autoload.php
@@ -1,10 +1,12 @@
 <?php
 
+include __DIR__ . '/vendor/autoload.php';
+
 spl_autoload_register(function ($className) {
 
-    $directory[] = __DIR__ . '/../extension';
-    $directory[] = __DIR__ . '/../library';
-    $directory[] = __DIR__ . '/../web/module';
+    $directory[] = __DIR__ . '/extension';
+    $directory[] = __DIR__ . '/library';
+    $directory[] = __DIR__ . '/web/module';
     foreach ($directory as $dir) {
 
         $file = $dir . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
--- web/dev.php
+++ web/dev.php
@@ -1,7 +1,9 @@
 <?php
-include __DIR__ . '/../core/start.php';
-
+ini_set('error_reporting', E_ALL | E_STRICT); // Типы ошибок, на которые надо ругаться
 ini_set('display_errors', true); // Вывод ошибок
+ini_set('error_log', __DIR__ . '/../log/error.log'); // Файл для сохранения ошибок
+
+include __DIR__ . '/../autoload.php';
 
 $Builder = new arhone\builder\Builder();
 $Builder->instruction(include __DIR__ . '/../config/builder.php');
