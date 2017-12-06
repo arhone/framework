4b82f4a52c89e5e491975a868a4fc6839f61203f
Алексей Арх <info@arh.one>
2017-12-06 21:52:39 +0300

    Framework

--- web/dev.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__ . '/../core/start.php';
-
-ini_set('display_errors', true); // Вывод ошибок
-
-$Builder = new arhone\builder\Builder();
-$Builder->instruction(include __DIR__ . '/../config/builder.php');
-
-echo $Builder->make('Controller')->run('HTTP:' . $_SERVER['REQUEST_METHOD'] . ':' . $_SERVER['REQUEST_URI'] ?? '/');
