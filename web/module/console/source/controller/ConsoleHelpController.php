7b95f8932fbcd8fd8199adb93f96856aed1d624b
Алексей Арх <a@arh.one>
2018-05-25 17:41:17 +0300

    framework

--- web/module/console/source/controller/ConsoleHelpController.php
+++ web/module/console/source/controller/ConsoleHelpController.php
@@ -12,12 +12,13 @@ class ConsoleHelpController {
      */
     public function help () {
 
-        $info[] = '--help, -h       - Выводит подсказку';
-        $info[] = '--develop, -d    - Добавляет в проект константу DEVELOP (arh cron -d)';
-        $info[] = '--test, -t       - Добавляет в проект константу TEST';
+        $info[] = "\033[32m" . ' -h, --help       ' . "\033[37m". '- Выводит подсказку';
+        $info[] = "\033[32m" . ' -d, --develop    ' . "\033[37m". '- Добавляет в проект константу DEVELOP (arh cron -d)';
+        $info[] = "\033[32m" . ' -t, --test       ' . "\033[37m". '- Добавляет в проект константу TEST';
         $info[] = '';
-        $info[] = 'cache-clear, cc  - Очищает весь кэш';
-        $info[] = 'cron             - Для запуска кода по расписанию (arh cron (1h, 1D, 1W, 1M, 1Y) / arh module_name cron)';
+        $info[] = "\033[33m" . '# команды';
+        $info[] = "\033[32m" . ' cron             ' . "\033[37m". '- Для запуска кода по расписанию (arh cron (1h, 1D, 1W, 1M, 1Y) / arh module_name cron)';
+        $info[] = "\033[32m" . ' cache:clear      ' . "\033[37m". '- Очищает весь кэш';
 
         return implode(PHP_EOL, $info) . PHP_EOL;
 
