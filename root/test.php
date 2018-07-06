ec43b459ae37e8d83525fd7d88e9457549de7b8d
Алексей Арх <a@arh.one>
2018-07-06 13:54:39 +0300

    dev

--- root/test.php
+++ root/test.php
@@ -1,3 +1,4 @@
 <?php
 
-(include __DIR__ . '/../include/arhone/core.php')->run('HTTP:' . ($_SERVER['REQUEST_METHOD'] ?? 'GET') . ':' . strtok($_SERVER['REQUEST_URI'] ?? '/', '?'));
+(include __DIR__ . '/../include/arhone/core.php')
+    ->run('HTTP:' . ($_SERVER['REQUEST_METHOD'] ?? 'GET') . ':' . strtok($_SERVER['REQUEST_URI'] ?? '/', '?'));
