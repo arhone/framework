6e165b09d509dfe214044b8d4e7c1a208e0a973a
Алексей Арх <a@arh.one>
2018-08-31 10:51:51 +0300

    dev

---
+++ config/arhone/project/migration.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'table_name' => [
+        0 => [
+            'up'   => function () {
+                return true;
+            },
+            'down' => function () {
+                return true;
+            },
+        ],
+        1 => [
+            'up'   => function () {
+                return true;
+            },
+            'down' => function () {
+                return true;
+            },
+        ]
+    ]
+];
---
+++ module/console/source/controller/ConsoleMigrationController.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace console\controller;
+
+/**
+ * Class ConsoleMigrationController
+ * @package console\controller
+ * @author Алексей Арх <info@arh.one>
+ */
+class ConsoleMigrationController {
+
+
+
+}
